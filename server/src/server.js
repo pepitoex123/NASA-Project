584ef8a02309daf4e270720e0c8725e66505baeb
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-07 22:06:58 -0300

    added mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,27 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 const {loadPlanetData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://nasa-api:s7byTxUFMWQyq52i@cluster0.xiz6j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+    console.log("MongoDB connection ready!");
+})
+
+mongoose.connection.on("error",(err) => {
+    console.error(err);
+})
 
 async function startServer(){
+    await mongoose.connect(MONGO_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     await loadPlanetData();
     server.listen(PORT,() => {
         console.log(`Listening on ${PORT}`);
